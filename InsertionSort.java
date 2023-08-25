a34bb7123744e3501595278ec3f42bb6a86f4893
jyoti kumari <jk648892@gmail.com>
2023-08-25 21:48:54 +0530

    InsertionSort

---
+++ InsertionSort.java
@@ -0,0 +1,20 @@
+public class InsertionSort {
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[]args){
+        int arr[]={4,8,1,5,2};
+        for(int i=1;i<arr.length;i++){
+            int temp=arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>temp){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=temp;
+        }
+        printArray(arr);
+    }
+}
